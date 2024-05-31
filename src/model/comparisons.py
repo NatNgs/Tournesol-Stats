a0b6f77379ad389ca680ade4850dd13ee4dcecf5
Nathaël NOGUÈS <nathael.nogues@gmail.com>
2024-05-31 14:30:39 +0200

    Updated input data format & improved notebooks

--- src/model/comparisons.py
+++ src/model/comparisons.py
@@ -1,14 +1,15 @@
 from typing import Callable
 
 class ComparisonLine:
-	def __init__(self, sp: list[str]):
+	def __init__(self, sp: dict[str,str]):
 		try:
-			self.user = sp[0]
-			self.vid1 = sp[1]
-			self.vid2 = sp[2]
-			self.criterion = sp[3]
-			self.score = int(float(sp[4]))
-			self.date = sp[5]
+			# score,score_max,week_date
+			self.user = sp['public_username']
+			self.vid1 = sp['video_a']
+			self.vid2 = sp['video_b']
+			self.criterion = sp['criteria']
+			self.score = int(float(sp['score']))
+			self.date = sp['week_date']
 		except:
 			print(sp)
 			raise
@@ -21,14 +22,18 @@ class ComparisonFile:
 	def foreach(self, fn: Callable[[ComparisonLine], None]):
 		# public_username,video_a,video_b,criteria,score
 		cmpFile = open(self.file_location, 'r', encoding='utf-8')
-		# Skip first line (headers)
-		cmpFile.readline()
+
+		# First line (headers)
+		firstline = list(map(lambda s: s.strip(), cmpFile.readline().split(',')))
 
 		while True:
 			line = cmpFile.readline()
 			# if line is empty, end of file is reached
 			if not line:
 				break
-			fn(ComparisonLine(line.strip().split(',')))
+
+			spt = line.split(',')
+			dta = {firstline[i]: spt[i].strip() for i in range(len(spt))}
+			fn(ComparisonLine(dta))
 
 		cmpFile.close()
